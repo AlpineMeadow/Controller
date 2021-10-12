0bd9441135b4bfefb38c1015f288556b7cfb41b9
jdw <jdw.space@gmail.com>
2021-10-12 03:42:42 -0600

    Added the latitude, longitude and altitude to the program as constants so that I do not need a function to obtain them as I am unable to access the internet at the AFA.

--- QPT.py
+++ QPT.py
@@ -11,7 +11,7 @@ def main() :
   import serial
   import QPTFunctions as QPTF
   import getParams as gP
-  from geopy.geocoders import Nominatim
+#  from geopy.geocoders import Nominatim
   
   #Set up the serial port.
   port = '/dev/ttyUSB1'
@@ -21,55 +21,59 @@ def main() :
   timeout = 1  #Not sure what is the best value for this.  I might want to set this to some
   #floating point value.
 
-#  ser = serial.Serial(
-#    port = port,
-#    baudrate = baud_rate,
-#    bytesize = byte_size,
-#    parity = parity,
-#    timeout = timeout)
+  ser = serial.Serial(
+    port = port,
+    baudrate = baud_rate,
+    bytesize = byte_size,
+    parity = parity,
+    timeout = timeout)
     
   #Fill the PARAMS dataclass.
   PARAMS = gP.getParams(QPTF.getArgs(argparse.ArgumentParser()))
   
   #Get the latitude and longitude.
-  locator = Nominatim(user_agent = 'QPT')
-  location = locator.geocode(PARAMS.location)
-  latitude = location.latitude
-  longitude = location.longitude
-  
+#  locator = Nominatim(user_agent = 'QPT')
+#  location = locator.geocode(PARAMS.location)
+#  latitude = location.latitude
+#  longitude = location.longitude
+
+  latitude = 38.9983  #Air Force Academy
+  longitude = -104.8613  #Air Force Academy
+  altitude = 2068.982  #Air Force Academy. Unit is meters.  This corresponds to 6788 ft.
+
   #Get the altitude.
-  altitude = QPTF.getElevation(latitude, longitude)
+#  altitude = QPTF.getElevation(latitude, longitude)
 
   #Fill in the Parameter data class values for latitude, longitude and altitude.
   PARAMS.latitude = latitude
   PARAMS.longitude = longitude
   PARAMS. altitude = altitude
 
-  breakpoint()
+#  breakpoint()
   
-  if(Command == 'send status jog') :
+  if(PARAMS.command == 'send status jog') :
     QPTF.sendStatusJog(PARAMS, ser)
-  #End of if statement - if(Command == 'SendStatusJog') :
+  #End of if statement - if(PARAMS.command == 'SendStatusJog') :
   
-  if(Command == 'send stop') :
+  if(PARAMS.command == 'send stop') :
     QPTF.sendStop(PARAMS, ser)
-  #End of if statement- if(Command == 'SendStatusJog') :
+  #End of if statement- if(PARAMS.command == 'SendStatusJog') :
     
-  if(Command == 'move to zero zero') :
+  if(PARAMS.command == 'move to zero zero') :
     QPTF.moveToZeroZero(PARAMS, ser)
-  #End of if statement - if(Command == 'SendStatusJog') :
+  #End of if statement - if(PARAMS.command == 'SendStatusJog') :
 
-  if(Command == 'move to entered coords') :
+  if(PARAMS.command == 'move to entered coordinates') :
     QPTF.moveToEnteredCoords(PARAMS, ser)
-  #End of if statement - if(Command == 'SendStatusJog') :
+  #End of if statement - if(PARAMS.command == 'SendStatusJog') :
     
-  if(Command == 'move to delta coords') :
+  if(PARAMS.command == 'move to delta coords') :
     QPTF.moveToDeltaCoords(PARAMS, ser)
-  #End of if statement - if(Command == 'SendStatusJog') :
+  #End of if statement - if(PARAMS.command == 'SendStatusJog') :
 
-  if(Command == 'move to home') :
+  if(PARAMS.command == 'move to home') :
     QPTF.moveToHome(PARAMS, ser)
-  #End of if statement -  if(Command == 'MoveToHome')
+  #End of if statement -  if(PARAMS.command == 'MoveToHome')
 
   #Close the serial port.
   ser.close()
